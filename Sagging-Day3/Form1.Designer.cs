d5cd0f17f31f876a61370782181bb9300d5e0c3a
matijatomc <55134628+matijatomc@users.noreply.github.com>
2023-05-12 16:48:19 +0200

    Add files via upload

---
+++ Sagging-Day3/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Sagging_Day3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.lblFile1 = new System.Windows.Forms.Label();
+            this.lblFile2 = new System.Windows.Forms.Label();
+            this.lblFile3 = new System.Windows.Forms.Label();
+            this.lblFile4 = new System.Windows.Forms.Label();
+            this.lblFile5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(150, 77);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(150, 230);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(335, 232);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(100, 20);
+            this.txtFileName.TabIndex = 4;
+            // 
+            // lblFile1
+            // 
+            this.lblFile1.AutoSize = true;
+            this.lblFile1.Location = new System.Drawing.Point(332, 25);
+            this.lblFile1.Name = "lblFile1";
+            this.lblFile1.Size = new System.Drawing.Size(37, 13);
+            this.lblFile1.TabIndex = 5;
+            this.lblFile1.Text = "1st file";
+            // 
+            // lblFile2
+            // 
+            this.lblFile2.AutoSize = true;
+            this.lblFile2.Location = new System.Drawing.Point(332, 57);
+            this.lblFile2.Name = "lblFile2";
+            this.lblFile2.Size = new System.Drawing.Size(41, 13);
+            this.lblFile2.TabIndex = 6;
+            this.lblFile2.Text = "2nd file";
+            // 
+            // lblFile3
+            // 
+            this.lblFile3.AutoSize = true;
+            this.lblFile3.Location = new System.Drawing.Point(332, 86);
+            this.lblFile3.Name = "lblFile3";
+            this.lblFile3.Size = new System.Drawing.Size(38, 13);
+            this.lblFile3.TabIndex = 7;
+            this.lblFile3.Text = "3rd file";
+            // 
+            // lblFile4
+            // 
+            this.lblFile4.AutoSize = true;
+            this.lblFile4.Location = new System.Drawing.Point(332, 115);
+            this.lblFile4.Name = "lblFile4";
+            this.lblFile4.Size = new System.Drawing.Size(38, 13);
+            this.lblFile4.TabIndex = 8;
+            this.lblFile4.Text = "4th file";
+            // 
+            // lblFile5
+            // 
+            this.lblFile5.AutoSize = true;
+            this.lblFile5.Location = new System.Drawing.Point(332, 145);
+            this.lblFile5.Name = "lblFile5";
+            this.lblFile5.Size = new System.Drawing.Size(38, 13);
+            this.lblFile5.TabIndex = 9;
+            this.lblFile5.Text = "5th file";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(270, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "List of files:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(252, 235);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "New file name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblFile5);
+            this.Controls.Add(this.lblFile4);
+            this.Controls.Add(this.lblFile3);
+            this.Controls.Add(this.lblFile2);
+            this.Controls.Add(this.lblFile1);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Infineon";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Label lblFile1;
+        private System.Windows.Forms.Label lblFile2;
+        private System.Windows.Forms.Label lblFile3;
+        private System.Windows.Forms.Label lblFile4;
+        private System.Windows.Forms.Label lblFile5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Sagging-Day3/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Globalization;
+
+namespace Sagging_Day3
+{
+    public partial class Form1 : Form
+    {
+        string[] filePath = new string[5];
+        int brc = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "JSON Files (*.json)|*.json";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK && brc < 5)
+                {
+                    for(int i = 0; i < 5; i++)
+                    {
+                        if (filePath[i] == openFileDialog.FileName) return;
+                    }
+                    filePath[brc] = openFileDialog.FileName;
+                    brc++;
+                }
+            }
+
+            if (filePath[0] != null) lblFile1.Text = filePath[0];
+            if (filePath[1] != null) lblFile2.Text = filePath[1];
+            if (filePath[2] != null) lblFile3.Text = filePath[2];
+            if (filePath[3] != null) lblFile4.Text = filePath[3];
+            if (filePath[4] != null) lblFile5.Text = filePath[4];
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
